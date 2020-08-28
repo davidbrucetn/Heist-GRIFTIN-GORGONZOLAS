c351693e529da55f5cbd8a3e36333b39888a8348
MandasK <king.amanda.r@gmail.com>
2020-08-28 10:27:57 -0500

    phase 1 complete

--- Heist/Program.cs
+++ Heist/Program.cs
@@ -6,7 +6,15 @@ namespace Heist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TeamMember newMember = new TeamMember();
+            Console.WriteLine("Plan your Heist!");
+            Console.Write("Enter team member name:");
+            newMember.Name = Console.ReadLine();
+            Console.Write("Enter skill level:");
+            newMember.skillLevel = int.Parse(Console.ReadLine());
+            Console.Write("Enter courage factor:");
+            newMember.courage = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{newMember.Name} {newMember.skillLevel} {newMember.courage}");
         }
     }
 }
---
+++ Heist/TeamMember.cs
@@ -0,0 +1,10 @@
+namespace Heist
+{
+    public class TeamMember
+    {
+        public string Name { get; set; }
+        public int skillLevel { get; set; }
+
+        public double courage { get; set; }
+    }
+}
